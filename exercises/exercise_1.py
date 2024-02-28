819a845a78ce53de629fd8ef0008198c766be3c9
Ibrahim12047 <161601167+Ibrahim12047@users.noreply.github.com>
2024-02-28 13:16:13 +0000

    code

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,21 @@
-# Exercise 1
-# Your solution comes here
+number = input("enter a number ") 
+def new_number(number):
+
+  num1 = number // 10000
+  num2 = (number // 1000) % 10
+  num3 = (number // 100) % 10
+  num4 = (number // 10) % 10
+  num5 = number % 10
+
+
+  newnum_1 = num1 + num3 + num5
+  newnum_2 = num2 + num4
+
+ 
+  new_number = int(str(newnum_1) + str(newnum_2))
+
+  return new_number
+
+newnumber = new_number(number)
+print(new_number)
+
