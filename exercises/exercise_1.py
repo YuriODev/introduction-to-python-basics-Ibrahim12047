9cb5e66db57486bb22d62fea1a77c333c65a1720
Ibrahim12047 <161601167+Ibrahim12047@users.noreply.github.com>
2024-02-28 13:23:02 +0000

    changed

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,21 +1,13 @@
-number = input("enter a number ") 
-def new_number(number):
-
+def get_new_number(number):
   num1 = number // 10000
   num2 = (number // 1000) % 10
   num3 = (number // 100) % 10
   num4 = (number // 10) % 10
   num5 = number % 10
-
-
   newnum_1 = num1 + num3 + num5
   newnum_2 = num2 + num4
 
- 
-  new_number = int(str(newnum_1) + str(newnum_2))
-
+  new_number = newnum_1 * 10 + newnum_2
   return new_number
-
-newnumber = new_number(number)
+new_number = get_new_number(int(input("Enter a five-digit number: ")))
 print(new_number)
-
