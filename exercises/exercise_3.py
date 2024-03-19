a6dff78c70cadc21143debbfa3ba4e066b0bb810
Ibrahim12047 <161601167+Ibrahim12047@users.noreply.github.com>
2024-03-19 12:09:17 +0000

    ex3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,6 +1,6 @@
 # Exercise 3
 # Your solution comes here
-n = int(input("Enter the number of seconds that have passed since the beginning of the day: ")) 
+n = int(input()) 
 hours = n // 3600
 minutes = (n % 3600) // 60
 seconds = n % 60
