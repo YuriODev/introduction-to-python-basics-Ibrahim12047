a5046d1ced1fd6b9b60a97a1e988222dad4910b6
Ibrahim12047 <161601167+Ibrahim12047@users.noreply.github.com>
2024-03-19 12:01:06 +0000

    a

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,8 +1,10 @@
 # Exercise 8
 # Your solution comes here
-def sort_three_integers(a, b, c):
-  minimum = min(a, b, c)
-  maximum = max(a, b, c)
-  middle = (a + b + c) - minimum - maximum
-  return (minimum, middle, maximum)
-print(sort_three_integers(3, 1, 2))
+a=input()
+b=input()
+c=input()
+minimum = min(a, b, c)
+maximum = max(a, b, c)
+middle = (a + b + c) - minimum - maximum
+sorted_numbers = (minimum, middle, maximum)
+print(sorted_numbers)
